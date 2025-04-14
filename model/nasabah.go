dadb5404212e5853a9a204eb488647e94ff73bc0
TnAhonk12 <aputriaisyah12@gmail.com>
2025-04-15 00:59:56 +0700

    update primary key

--- model/nasabah.go
+++ model/nasabah.go
@@ -1,13 +1,10 @@
 package model
 
-import "gorm.io/gorm"
-
 type Nasabah struct {
-	gorm.Model
+	NoRekening string      `json:"no_rekening" gorm:"primaryKey"`
 	Nama       string      `json:"nama"`
 	NIK        string      `json:"nik" gorm:"uniqueIndex"`
 	NoHP       string      `json:"no_hp" gorm:"uniqueIndex"`
-	NoRekening string      `json:"no_rekening" gorm:"uniqueIndex"`
 	Saldo      int64       `json:"saldo"`
 	Transaksi  []Transaksi `gorm:"foreignKey:NoRekening;references:NoRekening"`
 }
--- model/transaksi.go
+++ model/transaksi.go
@@ -1,11 +1,8 @@
 package model
 
-import "gorm.io/gorm"
-
 type Transaksi struct {
-	gorm.Model
 	NoRekening string `json:"no_rekening"`
-	Tipe       string `json:"tipe"` // "tabung" / "tarik"
+	Tipe       string `json:"tipe"`
 	Nominal    int64  `json:"nominal"`
 	Saldo      int64  `json:"saldo"`
 }
