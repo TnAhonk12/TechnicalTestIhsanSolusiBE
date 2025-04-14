ca23f03519e8418e3b0cd3a009eafd52cd8579eb
TnAhonk12 <aputriaisyah12@gmail.com>
2025-04-15 00:31:06 +0700

    nambah middleware

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/TnAhonk12/TechnicalTestIhsanSolusiBE/model"
 	"github.com/TnAhonk12/TechnicalTestIhsanSolusiBE/router"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/joho/godotenv"
 )
 
@@ -19,6 +21,10 @@ func main() {
 	db.DB.AutoMigrate(&model.Nasabah{}, &model.Transaksi{})
 
 	app := fiber.New()
+
+	app.Use(logger.New())
+	app.Use(recover.New())
+
 	router.Setup(app)
 
 	log.Printf("[INFO] Server running at port %s", cfg.AppPort)
